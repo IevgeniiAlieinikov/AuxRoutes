ac7fbce91bfcfa836ce7775019cb6fc18f405b81
Ievgenii <ievgenii_alieinikov@epam.com>
2019-08-13 13:57:53 +0300

    adding some styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular Router Demo';
+  showModal = true;
+
+  constructor(private _router: Router, private _route: ActivatedRoute) {}
+
+  openModal() {
+    this.showModal = true;
+  }
+
+  openInvestorModal() {
+    this.openModal();
+    this._router.navigate(['.', { outlets: { modal: ['investor-view'] } }], {
+      relativeTo: this._route.parent
+    });
+  }
+
+  openProductModal() {
+    this.openModal();
+    this._router.navigate(['.', { outlets: { modal: ['product', 'list'] } }], {
+      relativeTo: this._route.parent
+    });
+  }
+
+  closeModal() {
+    this._router.navigate(['.', { outlets: { modal: null } }], {
+      relativeTo: this._route.parent
+    });
+
+    this.showModal = false;
+  }
 }
