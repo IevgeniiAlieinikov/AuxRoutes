91acdfe68fd8c736e8c9cf5d74d2d400d988c524
Ievgenii <ievgenii_alieinikov@epam.com>
2019-08-13 14:43:31 +0300

    fixing root routes issue

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,18 +1,23 @@
 import { RouterModule, Routes } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 
+
 const routes: Routes = [
   {
     path: 'product',
     loadChildren: () => import(`./product/product.module`).then(m => m.ProductModule)
   },
   {
-    path: 'investor',
-    loadChildren: () => import(`./investor/investor.module`).then(m => m.InvestorModule)},
+    path: 'investor-view',
+    loadChildren: () => import(`./investor/investor.module`).then(m => m.InvestorModule)
+  },
   {
-    path: '',
+    path: '**',
     loadChildren: () => import(`./shared/shared.module`).then(m => m.SharedModule),
     outlet: 'modal',
+  },
+  {
+    path: '**', redirectTo: '/product/list'
   }
 ];
 
